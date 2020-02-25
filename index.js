4bf4be03692f25d8f4db7b58a9a84827504edeea
Jean Ya-Ching Yang <bdlas321@gmail.com>
2020-02-25 12:01:58 +0100

    feat(server): logger middleware

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 const express = require('express');
+const morgan = require('morgan');
+
 const apiRoutes = require('./api');
 
 const app = express();
 const PORT = 3000;
+const loggerType = process.env['development'] === 'true' ? 'dev' : 'common';
+
+/* log requests */
+app.use(morgan(loggerType, {
+  skip: function skip(req) {
+    return req.path.startsWith('/health') || req.path.startsWith('/version')
+  },
+}))
 
 /* all API endpoints */
 app.use('/api', apiRoutes);
