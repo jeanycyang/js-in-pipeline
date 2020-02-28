a6fea4fe09a8d9116bda70fcf0ab66169a03a652
Jean Ya-Ching Yang <bdlas321@gmail.com>
2020-02-28 18:25:35 +0100

    test(gift-code-generator): complete test cases

--- tests/unit/utils/giftCodeGenerator.test.js
+++ tests/unit/utils/giftCodeGenerator.test.js
@@ -6,4 +6,17 @@ describe('giftCodeGenerator', () => {
     expect(() => giftCodeGenerator(12, 'ccc')).toThrow(new Error('Length must be a number.'));
     expect(() => giftCodeGenerator('aaa', 'bbb')).toThrow(new Error('Length must be a number.'));
   });
+  it('should generate a gift code using default parameters', () => {
+    expect(giftCodeGenerator().length).toEqual(10);
+  });
+  it('should generate a gift code with correct length', () => {
+    expect(giftCodeGenerator(2, 2).length).toEqual(4);
+  });
+  it('should generate a gift code correctly', () => {
+    const giftCode = giftCodeGenerator(2, 2);
+    expect(giftCode[0]).toMatch(/[a-zA-Z]/);
+    expect(giftCode[1]).toMatch(/[a-zA-Z]/);
+    expect(giftCode[2]).toMatch(/[0-9]/);
+    expect(giftCode[3]).toMatch(/[0-9]/);
+  });
 });
